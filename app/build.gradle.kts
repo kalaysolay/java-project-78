9f92096040a06004be4df8798335e829792c0d1a
kalaysolay <ashat_boztaev@mail.ru>
2025-04-15 08:32:54 +0500

    initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("application")
+    id("checkstyle")
+    id("org.sonarqube") version "6.1.0.5360"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "kalaysolay_java-project-71")
+        property("sonar.organization", "kalaysolay")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    gradlePluginPortal()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
